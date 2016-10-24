5746dd4b421eba04c4b8346b7238858ffa2d7004
Jane Moskvina <jane.moskvina@gmail.com>
2016-10-24 20:24:27 +0300

    lesson 14

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by jane on 10/24/16.
+ */
+public class Square {
+
+  public double l;
+
+  public Square (double l) {
+    this.l = l;
+  }
+
+  public double area(){
+    return this.l * this.l;
+  }
+
+}
