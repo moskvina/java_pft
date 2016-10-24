790f5216efdf3bc9b28d63c74cdd6fd1475deae0
Jane Moskvina <jane.moskvina@gmail.com>
2016-10-24 16:36:57 +0300

    Package for my first program is created

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,8 +1,10 @@
 public class MyFirstProgram {
     
     public static void main(String[] args){
-        
-System.out.println("Hello, world!");
+      System.out.println("Hello, " + "world!");
+
+
+      System.out.println("Площадь квадрата со стороной" + 5 + " = " + (5 * 5));
 
 }
 }
