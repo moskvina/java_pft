e67523de2dbde03872aa342a7f6e35015d4688d1
Jane Moskvina <jane.moskvina@gmail.com>
2016-10-28 14:07:16 +0300

    test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -10,8 +10,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by jane on 10/28/16.
  */
-public class ApplicationManager {
-  FirefoxDriver wd;
+public class ApplicationManager extends GroupHelper {
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
       try {
@@ -39,32 +38,6 @@ public class ApplicationManager {
       wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
-  public void returnToGroupPage() {
-      wd.findElement(By.linkText("group page")).click();
-  }
-
-  public void submitGroupCreation() {
-      wd.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-      wd.findElement(By.name("group_name")).click();
-      wd.findElement(By.name("group_name")).clear();
-      wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-      wd.findElement(By.name("group_header")).click();
-      wd.findElement(By.name("group_name")).click();
-      wd.findElement(By.name("group_header")).click();
-      wd.findElement(By.name("group_header")).clear();
-      wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-      wd.findElement(By.name("group_footer")).click();
-      wd.findElement(By.name("group_footer")).clear();
-      wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-      wd.findElement(By.name("new")).click();
-  }
-
   public void gotoGroupPage() {
       wd.findElement(By.linkText("groups")).click();
   }
@@ -73,11 +46,4 @@ public class ApplicationManager {
     wd.quit();
   }
 
-  public void deleteSelectedGroups() {
-      wd.findElement(By.name("delete")).click();
-  }
-
-  public void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
-  }
 }
