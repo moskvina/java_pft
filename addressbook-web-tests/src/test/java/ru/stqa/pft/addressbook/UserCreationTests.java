535b866f6ba65b52f3291cc92dd2fbb8892185ac
Jane Moskvina <jane.moskvina@gmail.com>
2016-10-28 14:21:44 +0300

    Revert to Lesson7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
@@ -1,30 +1,13 @@
-package ru.stqa.pft.addressbook.tests;
+package ru.stqa.pft.addressbook;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-<<<<<<< HEAD:addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
-<<<<<<< HEAD:addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
-<<<<<<< HEAD:addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
-import ru.stqa.pft.addressbook.model.UserData;
-=======
-import static org.openqa.selenium.OutputType.*;
->>>>>>> parent of d722a60... UserCreationTests - refactoring:addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
-=======
-import static org.openqa.selenium.OutputType.*;
->>>>>>> parent of d722a60... UserCreationTests - refactoring:addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
-=======
-import static org.openqa.selenium.OutputType.*;
->>>>>>> parent of d722a60... UserCreationTests - refactoring:addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
 
 public class UserCreationTests {
     FirefoxDriver wd;
@@ -34,23 +17,23 @@ public class UserCreationTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/index.php");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String usename, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(usename);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
     public void testUserCreation() {
         initUserCreation();
-        fillUserForm();
+        fillUserForm(new UserData("Test1", "Test2", "Ukraine", "+3809711110001", "test@gmail.com"));
         submitUserCreation();
         returnToHomePage();
     }
@@ -63,24 +46,22 @@ public class UserCreationTests {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillUserForm() {
+    private void fillUserForm(UserData userData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Test1");
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).sendKeys("\\9");
+        wd.findElement(By.name("firstname")).sendKeys(userData.getFirstname());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Test2");
+        wd.findElement(By.name("lastname")).sendKeys(userData.getLastname());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("Ukraine");
+        wd.findElement(By.name("address")).sendKeys(userData.getAddress());
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("+3809711110001");
+        wd.findElement(By.name("home")).sendKeys(userData.getHomenumber());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("test@gmail.com");
+        wd.findElement(By.name("email")).sendKeys(userData.getEmail());
     }
 
     private void initUserCreation() {
