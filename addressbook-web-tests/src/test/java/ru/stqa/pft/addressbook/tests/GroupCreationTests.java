d442adfb6e92d0d8c36f20c3611df4c503bbfb2f
Jane Moskvina <jane.moskvina@gmail.com>
2016-11-10 15:32:25 +0200

    GroupCreation - Group count class added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -7,8 +8,10 @@ public class GroupCreationTests  extends TestBase {
 
     @Test
     public void testGroupCreation() {
-        app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().createGroup(new GroupData("testjane", null, null));
+      app.getNavigationHelper().gotoGroupPage();
+      int before = app.getGroupHelper().getGroupCount();
+      app.getGroupHelper().createGroup(new GroupData("testjane", null, null));
+      int after = app.getGroupHelper().getGroupCount();
+      Assert.assertEquals(after, before + 1);
     }
-
 }
