9e44a1b09e336102663b3742df28cc947dfa12a7
Jane Moskvina <jane.moskvina@gmail.com>
2017-04-06 16:22:27 +0300

    UserPhoneTests with error

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserPhoneTests.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.UserData;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
  * Created by jane on 4/6/17.
  */
@@ -14,6 +17,10 @@ public class UserPhoneTests extends TestBase {
     app.goTo().homePage();
     UserData user = app.user().all().iterator().next();
     UserData userInfoFromEditForm = app.user().infoFromEditForm(user);
+
+    assertThat(user.getHomePhone(), equalTo(userInfoFromEditForm.getHomePhone()));
+    assertThat(user.getMobilePhone(), equalTo(userInfoFromEditForm.getMobilePhone()));
+    assertThat(user.getWorkPhone(), equalTo(userInfoFromEditForm.getWorkPhone()));
   }
 
 }
